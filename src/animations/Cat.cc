09462f1ead9abfd93c76b183de8767feb87c7b4b
Dylan McGannon <d0x2f@users.noreply.github.com>
2017-02-16 09:38:00 +1100

    Updated doc comments.

--- src/animations/Cat.cc
+++ src/animations/Cat.cc
@@ -10,14 +10,15 @@ using namespace Animate;
  *
  * @param gl_area       GTK OpenGL area reference.
  * @param gl_context    GTK Opengl context reference.
- *
- * @return True so as not to bubble into another render handler.
  */
 void Cat::on_render(GtkGLArea *gl_area, GdkGLContext *gl_context)
 {
     std::cout << "render" << std::endl;
 }
 
+/**
+ * Compute a tick
+ */
 void Cat::on_tick()
 {
 }
