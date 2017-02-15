4079c45f2fbd3c95a7265c1a0a83464f4f0d8d1d
Dylan McGannon <me@dyl.dog>
2017-02-15 20:42:57 +1100

    Doc comments for main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,13 @@
 #include "gui.hh"
 
+/**
+ * Uhhh.. makes it go.
+ *
+ * @param argc        The number of command line tokens given.
+ * @param argv        An array of command line tokens.
+ *
+ * @return The program result.
+ */
 int main(int argc, char **argv)
 {
     return gui_run(argc, argv);
