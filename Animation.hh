1573bc3307579556cbd890ef0ae91d2c90cebab7
Dylan McGannon <me@dyl.dog>
2017-02-25 14:34:10 +1100

    I like this notation better.

--- Animation.hh
+++ Animation.hh
@@ -15,9 +15,9 @@ namespace Animate
             Animation(Context *context);
             ~Animation();
 
-            virtual bool on_render(const Glib::RefPtr<Gdk::GLContext>& gl_context) {}
-            virtual void on_tick(GLuint64 time_delta) {}
-            virtual void on_realise() {}
+            virtual bool on_render(const Glib::RefPtr<Gdk::GLContext>& gl_context) = 0;
+            virtual void on_tick(GLuint64 time_delta) = 0;
+            virtual void on_realise() = 0;
 
             void run();
 
@@ -28,7 +28,7 @@ namespace Animate
             Context *context;
             int tick_rate = 60;
 
-            virtual void initialise() {}
+            virtual void initialise() = 0;
 
         private:
             std::unique_ptr<std::thread> tick_thread;
